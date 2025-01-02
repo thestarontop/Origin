7dbc8709a879f12ad9911dc4ec34aaf08fd839bb
xiaoyan114514 <138199486+xiaoyan114514@users.noreply.github.com>
2025-01-02 12:51:30 +0800

    vel

--- src/main/java/net/java/main/modules/combat/GrimNoXZVelocity.java
+++ src/main/java/net/java/main/modules/combat/GrimNoXZVelocity.java
@@ -23,7 +23,7 @@ import net.minecraft.world.entity.player.Player;
 
 public class GrimNoXZVelocity extends Module {
     public GrimNoXZVelocity() {
-        super("GrimNoXZVelocity","bzd",Module.Category.COMBAT);
+        super("Velocity","bzd",Module.Category.COMBAT);
     }
     private boolean a = false;
 
