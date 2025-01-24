16c509b52ec44fa5091de267660061cdda838f33
Star <407215275@qq.com>
2025-01-24 11:57:37 +0800

    nochange

--- src/main/java/net/java/main/protocol/nel/all/NSessionInfo.java
+++
@@ -1,9 +0,0 @@
-package net.java.main.protocol.nel.all;
-
-public class NSessionInfo {
-    public int port;
-    public String entityId;
-    public String username;
-    public String token;
-    public String playerGameUuid = "";
-}
