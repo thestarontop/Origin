4c6974fee5d1112f8f4f182aefa4750645bdb73e
Star <407215275@qq.com>
2025-01-27 13:57:35 +0800

    bzd

--- src/main/java/net/java/main/modules/movement/StrafeFix.java
+++ src/main/java/net/java/main/modules/movement/StrafeFix.java
@@ -25,25 +25,12 @@ public class StrafeFix extends Module {
     public void onStrafe(StrafeEvent event){
         if (RotationUtils.targetRotation == null) return;
         event.setVelocity(RotationUtils.fixVelocity(event.getVelocity(), event.getMovementinput(), event.getSpeed()));
-        event.setYaw(RotationUtils.targetRotation.getYaw());
-
     }
-
-
     @EventTarget
     public void onJump(JumpEvent event){
         if (RotationUtils.targetRotation == null) return;
 
         event.setYaw(RotationUtils.targetRotation.getYaw());
-    }
-    @EventTarget
-    public void onMotion(MotionEvent event){
-        if (RotationUtils.targetRotation == null) return;
-        if (event.getPost()) return;
-
-        event.setYaw(RotationUtils.targetRotation.getYaw());
-        event.setPitch(RotationUtils.targetRotation.getPitch());
-
     }
 
     @EventTarget
